36086b3ec48e3b5c871c4b34dad6df228aef0d24
Manjula <manjukhambal@gmail.com>
2021-04-10 23:15:39 +0530

    Added MVC And New Controller

--- Webgenal.BookStore/Startup.cs
+++ Webgenal.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Webgenal.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +26,24 @@ namespace Webgenal.BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
+            //Middlewares
+            //app.Use( async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("This is my first Middleware");
+
+            //   await next();
+            //});
+
+            //app.Use( async (context, next) =>
+            //{
+            //  await  context.Response.WriteAsync("this is my second middleware");
+            //});
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
